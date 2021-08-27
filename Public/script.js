1f5a051243d9b20b8317dd3615b54d4231de9b52
sofiamajjul <sofiamajjul14@gmail.com>
2021-08-26 22:44:08 -0300

    Update javascript

--- Public/script.js
+++ Public/script.js
@@ -89,7 +89,7 @@ const preencherFormulario = (endereco) => {
 const pesquisarCep = async() => {
     limparFormulario();
     const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     const dados = await fetch(url); 
     const endereco = await dados.json();
     if (endereco.hasOwnProperty('erro')){
