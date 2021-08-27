e4f49ec473211610a2591be40952b2fd4b22b9fc
sofiamajjul <sofiamajjul14@gmail.com>
2021-08-26 22:34:31 -0300

    merge js files

--- Public/script.js
+++ Public/script.js
@@ -67,3 +67,36 @@ submit.addEventListener('click', function(e) {
     validator.validate(form);
 
   });
+
+  //Validação CEP
+
+  const limparFormulario = (endereco) => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+    document.getElementById('numero').value = '';
+}
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+
+const pesquisarCep = async() => {
+    limparFormulario();
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const dados = await fetch(url); 
+    const endereco = await dados.json();
+    if (endereco.hasOwnProperty('erro')){
+        document.getElementById('endereco').value = 'CEP não encontrado:(';
+    }else{
+        preencherFormulario(endereco);
+    }   
+}
+
+document.getElementById('cep').addEventListener('focusout',pesquisarCep);
